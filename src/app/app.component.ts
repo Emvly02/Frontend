d82a65c7f1a48bcd3b626abc7f0ecd100f63520b
Emily <emilyrekewitz@gmx.de>
2024-06-18 22:58:23 +0200

    Neu1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidenavComponent } from "./components/sidenav/sidenav.component";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, SidenavComponent, DashboardComponent]
 })
 export class AppComponent {
   title = 'Frontend';
---
+++ src/app/components/to-do/to-do.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-to-do',
+  standalone: true,
+  imports: [],
+  templateUrl: './to-do.component.html',
+  styleUrl: './to-do.component.scss'
+})
+export class ToDoComponent {
+
+}

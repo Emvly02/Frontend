cf7da3df23ecfb3392485b9ab9878ca7f20a9b35
jolienchen <becker.jolien@outlook.de>
2024-06-29 03:32:48 +0200

    Kleine Änderung
    - Sidenav bei bei Dashboard, ToDo, Termine (app.coponents)
    - margin-left auf 19.5% (SideNav war bei 250px und 19.5% war sehr ähnlich)
    -Logout Button -> Führt nur zu /login, da noch keine accounts erstellt werden können
    -Links bei Navbar
    -Global CSS angepasst

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,33 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
 import { SidenavComponent } from "./components/sidenav/sidenav.component";
 import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
-    imports: [RouterOutlet, SidenavComponent, DashboardComponent]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  imports: [CommonModule, RouterOutlet, SidenavComponent, DashboardComponent]
 })
 export class AppComponent {
   title = 'Frontend';
+  showSidenar: boolean = true;
+
+  // Abonniert Ereignisse des Routers, um Änderungen in der Navigation zu überwachen
+  // Überprüft, ob das Ereignis das Ende einer Navigation darstellt
+  // Die Sidenar wird ausgeblendet, wenn die URL '/login' oder '/register' ist
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.showSidenar = !['/login', '/register'].includes(event.urlAfterRedirects);
+      }
+    });
+  }
+
+  logout() {
+    this.router.navigate(['/login']);
+  }
 }
--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss'
 })
